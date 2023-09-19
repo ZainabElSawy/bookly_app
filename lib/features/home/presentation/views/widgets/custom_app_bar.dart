c28a9549de47f53b60adb8e581c0e23d99e9e9c2
Zainab Hamdy <zeinabhamdy377@gmail.com>
2023-09-20 00:23:51 +0300

    add custom app bar

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,31 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 20,
+          ),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                CupertinoIcons.search,
+                size: 26,
+              )),
+        ],
+      ),
+    );
+  }
+}
