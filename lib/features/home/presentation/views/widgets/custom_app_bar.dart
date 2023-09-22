c78b53593297a8228e96ea882a00a73c4522d8ae
Zainab Hamdy <zeinabhamdy377@gmail.com>
2023-09-23 02:30:59 +0300

    create custom search text field

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({
@@ -19,7 +21,9 @@ class CustomAppBar extends StatelessWidget {
             height: 20,
           ),
           IconButton(
-              onPressed: () {},
+              onPressed: () {
+                GoRouter.of(context).push(AppRouter.kSearchView);
+              },
               icon: const Icon(
                 CupertinoIcons.search,
                 size: 26,
